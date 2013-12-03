89df784d35e871ea9712009ca2ee23234fe01953
fglukhov <f.glukhov@gmail.com>
2013-12-03 12:06:00 +0400

    Map pin added

--- js/map.js
+++ js/map.js
@@ -9,7 +9,16 @@ function init () {
         myPlacemark2 = new ymaps.Placemark([55.775353, 37.6168], {
             // Свойства.
             hintContent: '127473, г. Москва, 2-й Волконский переулок, д. 10'
-        });
+        },
+        {
+          iconImageHref: '../images/map-pin.png',
+          // Размеры метки.
+          iconImageSize: [157, 75],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-75, -70]
+        }
+        );
         
     myMap.controls
         // Кнопка изменения масштаба.
